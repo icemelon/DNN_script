d5729945266fe6dc59927186cf5b648489e882ae
Haichen Shen <haichen@cs.washington.edu>
2014-11-06 09:18:14 -0800

    update rsp parsing

--- rsp.py
+++ rsp.py
@@ -1,28 +1,47 @@
 from tokenparser import StringTokenParser
 
-def removeItem(arr, filter):
-	item = None
-	for e in arr:
-		if filter in e:
-			item = e
-			break
-	if item is not None:
-		arr.remove(item)
+class RspTemplate(object):
+	def __init__(self):
+		self.core = None
+		self.trainDataset = None
+		self.testDataset = None
+		self.classifier = None
+		self.rs = None
+		self.options = {}
 
-class RspDescription(object):
-	def __init__(self, rspFile, idrop, hdrop, dropEpoch):
-		self.firstEpoch = True
-		self.drop = False
-		self.idrop = idrop
-		self.hdrop = hdrop
-		self.dropEpoch = dropEpoch
-		self.options = []
-		self.classifier = {}
+	@staticmethod
+	def parseRspFile(rspTmplFile):
+		content = ""
+		with open(rspTmplFile) as f:
+			for line in f:
+				content += " " + line.strip()
+		tokens = StringTokenParser(content)
+		rspTmpl = RspTemplate()
+		while True:
+			token = tokens.pop(': ')
+			if token is None:
+				break
+			elif token == '/c':
+				rspTmpl.core = tokens.pop()
+				rspTmpl.trainDataset = tokens.pop()
+			elif token == '/test':
+				rspTmpl.testDataset = tokens.pop()
+			elif token == '/rs':
+				rspTmpl.rs = tokens.pop()
+			elif token == '/cl':
+				alg = tokens.pop()
+				options = RspTemplate.parseClassifier(tokens.pop())
+				rspTmpl.classifier = {'name': alg, 'options': options}
+			else:
+				if token.endswith('-'):
+					rspTmpl.options[token[:-1]] = '-'
+				else:
+					rspTmpl.options[token] = tokens.pop()
+		return rspTmpl
 
-		self.parse(rspFile)
-
-	def parseClassifier(self, description):
-		detail = {}
+	@staticmethod
+	def parseClassifier(description):
+		options = {}
 		tokens = StringTokenParser(description.strip('{}'))
 
 		while True:
@@ -31,105 +50,113 @@ class RspDescription(object):
 			if '=' in token:
 				lhs, rhs = token.split('=')
 				if lhs == 'bp':
-					detail['bp'] = {}
-					detail['bp']['name'] = rhs
-					desc = tokens.pop()
-					detail['bp']['detail'] = self.parseClassifier(desc)
+					alg = rhs
+					opt = RspTemplate.parseClassifier(tokens.pop())
+					options['bp'] = {'name': alg, 'options': opt}
 				else:
-					detail[lhs] = rhs
+					options[lhs] = rhs
 			else:
-				detail[token] = None
+				options[token] = None
 
-		return detail
-
-	def parse(self, rspFile):
-		with open(rspFile) as f:
-			for line in f:
-				tokens = StringTokenParser(line)
-				if tokens.pop() == '/cl':
-					self.classifier['name'] = tokens.pop()
-					self.classifier['detail'] = self.parseClassifier(tokens.pop())
-				else:
-					self.options.append(line.strip())
-		# print self.classifier
+		return options
 
 	def removeFirstEpoch(self):
-		if 'bp' in self.classifier['detail']:
-			mcnn = self.classifier['detail']['bp']
+		if 'bp' in self.classifier['options']:
+			cl_options = self.classifier['options']['bp']['options']
 		else:
-			mcnn = self.classifier
-		if 'initwts' in mcnn['detail']:
-			del mcnn['detail']['initwts']
-		if 'pretrain' in mcnn['detail']:
-			del mcnn['detail']['pretrain']
-		if 'prepoch' in mcnn['detail']:
-			del mcnn['detail']['prepoch']
-		self.firstEpoch = False
+			cl_options = self.classifier['options']
+		if 'initwts' in cl_options:
+			del cl_options['initwts']
+		if 'pretrain' in cl_options:
+			del cl_options['pretrain']
+		if 'prepoch' in cl_options:
+			del cl_options['prepoch']
 
-	def addDropout(self):
-		if 'bp' in self.classifier['detail']:
-			mcnn = self.classifier['detail']['bp']
+	def addDropout(self, idrop, hdrop):
+		if 'bp' in self.classifier['options']:
+			cl_options = self.classifier['options']['bp']['options']
 		else:
-			mcnn = self.classifier
-		if self.idrop is not None:
-			mcnn['detail']['idrop'] = self.idrop
-		if self.hdrop is not None:
-			mcnn['detail']['hdrop'] = self.hdrop
-		self.drop = True
+			cl_options = self.classifier['options']
+		if idrop is not None:
+			cl_options['idrop'] = idrop
+		if hdrop is not None:
+			cl_options['hdrop'] = hdrop
 
 	# output /cl line
 	def outputClassifier(self, lr, nn, cl=None):
 		if cl is None:
 			cl = self.classifier
 		s = '%s { ' % cl['name']
-		detail = cl['detail']
-
-		for key in sorted(detail.keys()):
+		options = cl['options']
+		for key in sorted(options.keys()):
 			if key == 'bp':
-				value = self.outputClassifier(lr, nn, detail[key])
+				value = self.outputClassifier(lr, nn, options[key])
+			elif key == 'lr':
+				value = lr
+			elif key == 'filename':
+				value = nn
 			else:
-				value = detail[key]
+				value = options[key]
 
 			if value is None:
-				format = '%s ' % key
+				s += '%s ' % key
 			else:
-				format = '%s=%s ' % (key, value)
-			if key == 'lr':
-				s += format % lr
-			elif key == 'filename':
-				s += format % nn
-			else:
-				s += format
+				s += '%s=%s ' % (key, value)
 		s += '}'
 		return s
 
-	def outputRspFile(self, task):
-		if self.firstEpoch and task.epoch > 0:
-			self.removeFirstEpoch()
-		if not self.drop and task.epoch >= self.dropEpoch:
-			self.addDropout()
+	def writeToFile(self, filename, lr, nn, rs, binModel, textModel):
+		with open(filename, 'w') as f:
+			f.write("/c %s %s\n" % (self.core, self.trainDataset))
+			if self.testDataset is not None:
+				f.write("/test %s\n" % self.testDataset)
+			for key in self.options:
+				val = self.options[key]
+				if val == '-':
+					f.write("%s%s\n" % (key, self.options[key]))
+				else:
+					f.write("%s %s\n" % (key, self.options[key]))
+			f.write("/cl %s\n" % self.outputClassifier(lr, nn))
+			if rs is None and self.rs is not None: rs = self.rs
+			if rs is not None: f.write("/rs %s\n" % rs)
+			f.write("/m %s\n" % binModel)
+			if textModel is not None: f.write("/mt %s\n" % textModel)
 
-		with open(task.rspFile, 'w') as f:
-			for line in self.options:
-				f.write('%s\n' % line)
-			f.write('/cl %s\n' % self.outputClassifier(task.lr, task.nn))
-			if task.rs is not None:
-				f.write('/rs:%s\n' % task.rs)
-			f.write('/m:%s\n' % task.binModel)
-			f.write('/mt:%s\n' % task.textModel)
+class RspWrapper(object):
+	def __init__(self, rspTmplFile, idrop, hdrop, dropEpoch):
+		self.rspTmpl = RspTemplate.parseRspFile(rspTmplFile)
+		self.idrop = idrop
+		self.hdrop = hdrop
+		self.dropEpoch = dropEpoch
+
+	def outputRspFile(self, task):
+		if task.epoch > 0:
+			self.rspTmpl.removeFirstEpoch()
+		if task.epoch >= self.dropEpoch:
+			self.rspTmpl.addDropout(self.idrop, self.hdrop)
+		self.rspTmpl.writeToFile(
+			task.rspFile, 
+			task.lr, 
+			task.nn, 
+			task.rs, 
+			task.binModel, 
+			task.textModel)
 
 if __name__ == '__main__':
 	import sys
-	rsp = RspDescription(sys.argv[1], .1, .5, 0)
+	# rspTmpl = RspTemplate.parseRspFile(sys.argv[1])
+	# rspTmpl.writeToFile("1.rsp", 0.1, 'a.nn', 1, 'b.bin', 'b.nn')
+	# exit()
+	rsp = RspWrapper(sys.argv[1], .1, .5, 0)
 	# exit()
 	class Task:
 		pass
 	task = Task()
 	task.epoch = 1
 	task.rs = 1
-	task.nn = 'aaa.nn'
+	task.nn = 'a0.nn'
 	task.lr = 0.1
-	task.binModel = 'a'
-	task.textModel = 'b'
-	task.rspFile = '1'
+	task.binModel = 'a1.bin'
+	task.textModel = 'a1.nn'
+	task.rspFile = '1.rsp'
 	rsp.outputRspFile(task)
